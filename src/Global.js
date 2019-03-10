8e2a76389ed795cf2506f9d80abf8e900753924a
thallysbezerra <bezerra.thallys@gmail.com>
2019-03-10 19:48:50 -0300

    Including Header and Footer

--- src/Global.js
+++ src/Global.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Header from "./widgets/Header/Header";
+import Footer from "./widgets/Footer/Footer";
 
 export default class Global extends Component {
-	render() {
-		return (
-			<div>
-				Teste
-			</div>
-		);				
-	}
-};
+    render() {
+        return (
+            <div>
+                <Header />
+                <Footer />
+            </div>
+        );
+    }
+}
